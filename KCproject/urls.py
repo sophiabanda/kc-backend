4b6febf08beb1a71373fe9d2dbe0eefbc86dfd7e
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-08-20 10:55:30 -0400

    removed line of code from urls file

--- KCproject/urls.py
+++ KCproject/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import re_path, path, include
-# from django.conf.urls import url
 from app.views import *
 
 urlpatterns = [
