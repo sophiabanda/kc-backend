58b8b129679c8ecf1a89e0465192bf7db4d9f963
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-08-20 12:42:15 -0400

    Updates across the board for name change of folder

--- kc-backend/wsgi.py
+++ kc-backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for KCproject project.
+WSGI config for kc-backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KCproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kc-backend.settings')
 
 application = get_wsgi_application()
